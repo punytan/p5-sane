dfe0b94aa758c030f3b33ca9a91eaacdbc58e0eb
punytan <punytan@gmail.com>
2011-10-20 01:01:13 +0900

    impl

--- lib/sane.pm
+++ lib/sane.pm
@@ -1,22 +1,44 @@
 package sane;
-use strict;
-use warnings;
 our $VERSION = '0.01';
+
+sub import {
+    require strict;
+    strict->import;
+
+    require warnings;
+    warnings->import;
+
+    require utf8;
+    utf8->import;
+
+    require feature;
+    feature->import(sprintf ":%vd", $^V);
+}
 
 1;
 __END__
 
 =head1 NAME
 
-sane -
+sane - provide sane default pragmas
 
 =head1 SYNOPSIS
 
   use sane;
 
+  # is the same as:
+
+  use strict;
+  use warnings;
+  use utf8;
+  use feature (sprintf(":%vd", $^V));
+
 =head1 DESCRIPTION
 
-sane is
+C<sane> pragma is written for people who are tired of typing hackneyed expressions.
+Every perl mongers write the same code on the top of scripts.
+
+Assuming that it is true, what we should do about it is shorten it.
 
 =head1 AUTHOR
 
